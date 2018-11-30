da0d13ed9c9700b756f87fc5c1dfc45d638e9019
yemiwebby <yemiwebby@gmail.com>
2018-11-30 13:48:27 +0100

    added a new sample file for the project

---
+++ src/components/sample.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SampleComponent extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is good </p>
+            </div>
+        )
+    }
+}
+
+export default SampleComponent;
